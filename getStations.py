f7c904e2bc50ec75786eb46a2bac3eba626d0160
Michael Pesek <pesekm@dupage.edu>
2020-10-04 18:37:58 -0500

    clicking links work, testing for text that starts with linebreaks

--- getStations.py
+++ getStations.py
@@ -9,6 +9,6 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get('http://www.radiosure.com/stations/')
 
 # click radio button
-python_button = driver.find_elements_by_partial_link_text('Down')
+python_button = driver.get_element_by_xpath(//*[normalize-space(@text)='Download updated stations database (updated on October 04, 2020).'])
 print(python_button)
 python_button.click()
