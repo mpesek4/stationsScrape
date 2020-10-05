0840f02e8e875eb07108c7aed525d6e33cd09517
Michael Pesek <pesekm@dupage.edu>
2020-10-05 16:51:20 -0500

    mapping puppeteer solution to selenium, for some reason the coordinates are off in the iframe

--- getStations.py
+++ getStations.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 import time
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
@@ -9,6 +12,21 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get('http://www.radiosure.com/stations/')
 
 # click radio button
-python_button = driver.get_element_by_xpath(//*[normalize-space(@text)='Download updated stations database (updated on October 04, 2020).'])
-print(python_button)
-python_button.click()
+# iframe = driver.find_elements_by_tag_name('iframe')[0]
+
+# print("iframe is", iframe)
+# driver.switch_to.frame(iframe)
+
+
+
+# driver.find_element_by_css_selector("body > b > b:nth-child(26) > a").click()
+
+
+
+WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR,"#post-264 > div > p:nth-child(2) > iframe")))
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "body > b > b:nth-child(26) > a"))).click()
+
+
+
+
+
